d55cdb628af1c12f1e77524b17c261191dd21b4f
rpo <64422494+naufalhakim23@users.noreply.github.com>
2022-01-08 21:57:17 +0700

    deleting credentials and api key

--- ESP32_Code/Idea/FinalWith Pump Logic.ino
+++ ESP32_Code/Idea/FinalWith Pump Logic.ino
@@ -36,21 +36,21 @@ DallasTemperature sensors(&oneWire);
 #include <addons/RTDBHelper.h>
 
 /* 1. Define the WiFi credentials */
-#define WIFI_SSID "MALUKU 24"
-#define WIFI_PASSWORD "2IkanCupang"
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
 
 //For the following credentials, see examples/Authentications/SignInAsUser/EmailPassword/EmailPassword.ino
 
 /* 2. Define the API Key */
-#define API_KEY "AIzaSyBevJHSAmYAcq5I9kTzoNgK1UodyTY9blw"
+#define API_KEY ""
 
 /* 3. Define the RTDB URL */
-#define DATABASE_URL "https://telegarden-5a048-default-rtdb.asia-southeast1.firebasedatabase.app" 
+#define DATABASE_URL "" 
  //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
 
 /* 4. Define the user Email and password that alreadey registerd or added in your project */
-#define USER_EMAIL "isozac@ymail.com"
-#define USER_PASSWORD "demaster"
+#define USER_EMAIL ""
+#define USER_PASSWORD ""
 
 
 //Define Firebase Data object
